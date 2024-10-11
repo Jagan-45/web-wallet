5d4ad3993465a5923dbb89752d0d8f3ce5266e11
Jagan-45 <balujgn@gmail.com>
2024-10-11 21:49:36 +0530

    added buffer import to see if that get's resolved in vercel

--- web_Wallet/client/src/hooks/useDecryptMnemonic.js
+++ web_Wallet/client/src/hooks/useDecryptMnemonic.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import crypto from 'crypto-browserify';
-
+import { Buffer } from 'buffer';
 const useDecryptMnemonic = (hashedPassword) => {
   const [loading, setIsLoading] = useState(true);
   const [decryptedData, setDecryptedData] = useState({ decryptedMnemonic: null, decryptedSeed: null });
